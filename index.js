27f568be1f3cac907864ee73568cab21916557f3
Dmitriy Nikanorov <dimonnikanorov2@gmail.com>
2021-08-08 19:13:38 +0300

    Add length(maybe)

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 /*Create new object*/
-const  object = {0:1, 1:2, 2:3, 3:4, 4:5,};
 
-const Object1 = Object.values(object);
+const  object = {0:1, 1:2, 2:3, 3:4, 4:5};
 
-console.log(object);
+const objectLength = Object.keys(object).length;
+
+const  objectPlusLength = {0:1, 1:2, 2:3, 3:4, 4:5, 'length':objectLength};
+
+const Object1 = Object.values(objectPlusLength);
+
+
+console.log(objectPlusLength)
 console.log(Object1)
-
