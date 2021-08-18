080bc6ae07714482f9a0456e05d7a483e70c738c
Dmitriy Nikanorov <dimonnikanorov2@gmail.com>
2021-08-18 19:49:04 +0300

    Create new class 'myArray'

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-/*Create new object*/
+class Array{
+    constructor(){
+        this.array = {};
+        for (let i = 0; i < arguments.length; i++) {
+            this.array[i] = arguments[i];
+        }
+        this.length = arguments.length;
+    }
+}
 
-const  object = {0:1, 1:2, 2:3, 3:4, 4:5};
-
-const objectLength = Object.keys(object).length;
-
-const  objectPlusLength = {0:1, 1:2, 2:3, 3:4, 4:5, 'length':objectLength};
-
-const Object1 = Object.values(objectPlusLength);
 
 
-console.log(objectPlusLength)
-console.log(Object1)
+const myArray = new Array (1, 2, 3, 4, 5);
+console.log(myArray);
